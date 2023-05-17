11f35280d82df415f4cd45e6d63f6ad43d8cc349
Juliana Villegas <jumavipe@hotmail.com>
2023-05-17 06:59:03 -0500

    added old unit test

--- src/components/sectionExampleComponent/sectionExampleComponent.spec.js
+++ src/components/sectionExampleComponent/sectionExampleComponent.spec.js
@@ -1,25 +1,23 @@
 import "./sectionExampleComponent.js";
 
-describe('SectionComponent', () => {
+describe('SectionComponent', ()=> {
 
-    it('renders the component and sets the params', () => {
+    it('renders the component and sets the params', ()=>{
 
-        // Arange
+        // ARRANGE
         const name = 'nameSection'
         const description = 'description'
         const section = document.createElement('section-example-component')
 
-        // Act
+        // ACT
         section.setAttribute('name', name)
         section.setAttribute('description', description)
-        section.setAttribute('background', 'cyan')
         document.body.append(section)
-
-        // Assert 
-        expect(section.querySelector('h2').textContent).toEqual('The hardcoded subtitle')
-        expect(section.querySelector('h1').textContent).toEqual(name)
-        expect(section.querySelector('p').textContent).toEqual(description)
-        expect(section.querySelector('section').style.backgroundColor).toEqual('cyan')
+        
+       // ASSERT 
+       expect(section.querySelector('h2').textContent).toEqual('The hardcoded subtitle')
+       expect(section.querySelector('h1').textContent).toEqual(name)
+       expect(section.querySelector('p').textContent).toEqual(description)
 
     })
 })
